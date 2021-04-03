b4c5b92ba28bb9f9809e4bf0284888958e735b40
Soumili-Ghosh3 <1930105@kiit.ac.in>
2021-04-03 19:39:18 +0530

    First Commit

---
+++ amazon-clone/src/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+//import styled from 'styled-components'
+
+function Header()
+{
+  return(
+      <div>
+         <Header />
+      </div>
+  );
+}
+
+export default Header
+
+/*const HeaderLogo = styled.div`
+    height: 60px;
+    background-color: black;
+`
+const HeaderOptionAddress = styled.div`
+    
+`*/
